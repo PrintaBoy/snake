1c7f409715ea9f7ac88dc5a720af68de12c1b4ba
PrintaBoy <printaboy@gmail.com>
2025-02-01 21:16:28 +0100

    Created basic Json parser with rudimentary saving and loading; (#2)
    Co-authored-by: PetrRuzicka <petr.ruzicka@divrlabs.com>

---
+++ Assets/Scripts/Utilities/GameData.cs
@@ -0,0 +1,18 @@
+[System.Serializable]
+public class GameData
+{
+    public static GameData gameData;
+    public int levelWidth;
+    public int levelHeight;
+    
+    public int currentScore;
+    public int bestScore;
+
+    public float snakeMovementSpeed;
+    public int snakeLength;
+
+    private void Awake()
+    {
+        gameData = this;
+    }
+}
---
+++ Assets/Scripts/Utilities/JSONParser.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.IO;
+
+public class JSONParser : MonoBehaviour
+{
+    private string jsonGameData;
+    private string jsonDataPath = Application.dataPath + "/Scripts/Utilities/GameData.json";
+
+    private void Awake()
+    {
+        LoadData();
+    }
+
+    public void SaveData()
+    {
+        jsonGameData = JsonUtility.ToJson(GameData.gameData);
+        File.WriteAllText(jsonDataPath, jsonGameData);
+    }
+
+    private void LoadData()
+    {
+        jsonGameData = File.ReadAllText(jsonDataPath);
+        GameData.gameData = JsonUtility.FromJson<GameData>(jsonGameData);
+    }
+}
