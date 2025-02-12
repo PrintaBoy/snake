862398211f9f3b2360c0a65d123cae4e2c01fe8d
PetrRuzicka <petr.ruzicka@divrlabs.com>
2025-02-12 21:37:32 +0100

    Snake now moves on it's own as well;

--- Assets/Scripts/Gameplay/Snake/SnakeController.cs
+++ Assets/Scripts/Gameplay/Snake/SnakeController.cs
@@ -7,7 +7,27 @@ public class SnakeController : MonoBehaviour
     [SerializeField] private List<SnakeSegment> snakeSegments;
     [SerializeField] private GameObject snakeSegmentPrefab;
 
+    private Directions lastCommandDirection = Directions.West;
+
+    private float movementSpeed = 0.1f;
+    private float doMoveTimer = 0f;
+    private float doMoveTimerMax = 1f; // refactor to JSON, load from gamedata
+
     public static event Action OnSnakeSpawned;
+
+    private void Start()
+    {
+        movementSpeed = GameData.gameData.snakeMovementSpeed;
+    }
+
+    private void Update()
+    {
+        doMoveTimer += Time.deltaTime * movementSpeed;
+        if (doMoveTimer >= doMoveTimerMax)
+        {
+            MoveSnake(lastCommandDirection);
+        }
+    }
 
     private void OnEnable()
     {
@@ -27,13 +47,17 @@ public class SnakeController : MonoBehaviour
     }
 
     public void MoveSnake(Directions newDirection) // receives from MoveCommand command to change direction of snake
-    {
+    {        
+
         IGridTile adjecentTileInDirection = snakeSegments[0].GetParent().GetAdjecentTile(newDirection);
 
         if (adjecentTileInDirection == snakeSegments[0].GetPreviousParent()) // this prevents snake to reverse into itself
         {
             return;
         }
+
+        lastCommandDirection = newDirection;
+        doMoveTimer = 0f;
 
         snakeSegments[0].CheckForCollision(adjecentTileInDirection);        
         snakeSegments[0].MoveSnakeSegment(adjecentTileInDirection);
--- Assets/Scripts/Gameplay/Snake/SnakeSegment.cs
+++ Assets/Scripts/Gameplay/Snake/SnakeSegment.cs
@@ -5,15 +5,6 @@ public class SnakeSegment : MonoBehaviour, ISpawnable
     private IGridTile parent;
     private IGridTile previousParent;
 
-    private float movementSpeed = 0.3f; 
-    private float doMoveTimer = 0f;
-    private float doMoveTimerMax = 0.3f; // refactor to JSON, load from gamedata
-
-    private void Start()
-    {
-        movementSpeed = GameData.gameData.snakeMovementSpeed;
-    }
-
     public void SetupSpawnable(IGridTile parentTile)
     {
         parent = parentTile;
@@ -27,18 +18,8 @@ public class SnakeSegment : MonoBehaviour, ISpawnable
         Debug.Log("Snake segment colision");
     }
 
-    private void Update() // mve timer to snakeController.cs
-    {
-        doMoveTimer += Time.deltaTime * movementSpeed;
-        if (doMoveTimer >= doMoveTimerMax)
-        {
-            //MoveSnakeSegment();
-        }
-    }
-
     public void MoveSnakeSegment(IGridTile tileToMoveTo)
-    {
-        doMoveTimer = 0f;       
+    {    
         parent.ClearChild();
         tileToMoveTo.BecomeParent(this);        
         gameObject.transform.position = tileToMoveTo.gameObject.transform.position;
